b01f7ebe44325ba92b0dc032f1e1bdada776f27e
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2022-03-23 18:44:04 +0000

    Create functioning CSV reader

--- src/index.js
+++ src/index.js
@@ -1,6 +1,26 @@
+import fs from 'fs'
+import { parse } from 'csv-parse'
 const convertWorkflowToUseSteps = require('./convertWorkflowToUseSteps.js')
 
-console.log('Hello')
+function main (inputFileName) {
+  if (!inputFileName) {
+    console.log('Please specify a workflow CSV')
+    return
+  }
+
+  fs.createReadStream(inputFileName)
+    .pipe(parse({delimiter: ','}))
+    .on('data', function(csvRow) {
+      console.log(csvRow)
+    })
+    .on('end',function() {
+      // Do nothing
+    })
+}
+
+const inputFileName = process.argv[2]
+
+main (inputFileName)
 
 /*
 const fs = require('fs')
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,14 @@
 const path = require('path')
 
 module.exports = {
+  target: 'node',
   entry: './src/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'app'),
+  },
+  resolve: {
+    fallback: {
+    }
   }
 }
